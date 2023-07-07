ba600e6ba7fdf1933ccd4a5877996ca58b0ebcf7
Matthias Beyer <mail@beyermatthias.de>
2023-07-07 10:53:54 +0200

    Import Optional trait+impls from typed-builder
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
 pub use typed_command_builder_derive::TypedCommandBuilder;
+
+#[doc(hidden)]
+pub trait Optional<T> {
+    fn into_value<F: FnOnce() -> T>(self, default: F) -> T;
+}
+
+impl<T> Optional<T> for () {
+    fn into_value<F: FnOnce() -> T>(self, default: F) -> T {
+        default()
+    }
+}
+
+impl<T> Optional<T> for (T,) {
+    fn into_value<F: FnOnce() -> T>(self, _: F) -> T {
+        self.0
+    }
+}
